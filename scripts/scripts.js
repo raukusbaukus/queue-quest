5d98b582247e5efa4ba83bfc4c574e56fdd76b79
bmatson <benjaminmatson@gmail.com>
2016-12-13 12:57:28 -0700

    improved tooltip, added hero selection, added ability to sewt hero stats

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,43 +1,81 @@
-$(document).ready(function(event) {
-  // commented out because doesn't work. should work
-  // var tooltiparr = [
-  //   $('#warpick'), $('#ranpick'), $('#magpick')
-  // ];
-  // console.log(tooltiparr[0]);
-  // for (let id of tooltiparr) {
-  //   tooltiparr[id].addEventListener('mouseenter', showtooltip);
-  //   tooltiparr[id].addEventListener('mouseleave', hidetooltip);
-  // }
-   $('body').mousemove(tooltipfollow);
-  $('#warpick').mouseenter(showtooltip);
-  $('#warpick').mouseleave(hidetooltip);
-  $('#ranpick').mouseenter(showtooltip);
-  $('#ranpick').mouseleave(hidetooltip);
-  $('#magpick').mouseenter(showtooltip);
-  $('#magpick').mouseleave(hidetooltip);
+var tooltipinfo = {
+  warpick: "The Warrior has the highest starting HP and Armor. They also begin play with the 'Repair' initiation ability, which lets you repair broken armor.",
+  ranpick: "The Ranger has the highest starting Energy and Base Damage. They also begin play with the 'Energize' initition ability, which lets you recover missing energy.",
+  magpick: "The Magi starts with a Ward to mitigate magic damage, and their attacks are considered magic damage, which bypasses armor. They also begin play with the 'Protect' initiation ability, which lets you recharge a diminished ward."
+};
 
+var tooltiparr = [];
+
+
+var heroinfo = {
+  maxhp: 30,
+  currhp: 30,
+  maxenergy: 20,
+  currenergy: 20,
+  type: 'Physical',
+  basedmg: 5,
+  dmgvariance: [1, 2],
+  maxarmor: 3,
+  currarmor: 3,
+  maxward: 0,
+  currward: 0,
+  name: '',
+  illus: 'images/characters/magi.gif'
+}
+
+$(document).ready(function(event) {
+  tooltiparr = [$('#warpick'), $('#ranpick'), $('#magpick')];
+
+  $('body').mousemove(tooltipfollow);
+  for (let id in tooltiparr) {
+    tooltiparr[id].mouseenter(showtooltip);
+    tooltiparr[id].mouseleave(hidetooltip);
+  }
+  $('#warpick').click(heropicked);
+  $('#ranpick').click(heropicked);
+  $('#magpick').click(heropicked);
 })
 
 function showtooltip() {
   $('#tooltip').text(tooltipinfo[this.id]);
-  $('#tooltip').css("display","inline");
+  $('#tooltip').css("display", "inline");
 }
 
 function hidetooltip() {
-  $('#tooltip').css("display","none");
+  $('#tooltip').css("display", "none");
 }
 
 function tooltipfollow() {
-  $('#tooltip').css("top",event.pageY);
-  $('#tooltip').css("left",event.pageX);
+  $('#tooltip').css("top", event.pageY);
+  $('#tooltip').css("left", event.pageX - $('#tooltip').width() / 2);
 }
 
-var tooltipinfo = {
-  warpick: "this is the warrior",
-  ranpick: "this is the ranger",
-  magpick: "this is the magi"
-};
+function heropicked() {
+  heroinfo['illus'] = this.src;
+  heroinfo['name'] = this.name;
+  console.log();
+  sethstats(heroinfo);
+}
 
-var tooltiparr = [
-  $('#warpick'), $('#ranpick'), $('#magpick')
-];
+function sethstats() {
+  $('#hero-pic').prop('src', heroinfo['illus']);
+  $('#hero-name').text(heroinfo['name']);
+  $('#hhp').text(`HP: ${heroinfo['currhp']}/${heroinfo['maxhp']}`);
+$('#hhpbar').prop('style',`width: ${Math.floor(heroinfo['currhp']/heroinfo['maxhp'])}`)
+  $('#henergy').text(`Energy: ${heroinfo['currenergy']}/${heroinfo['maxenergy']}`);
+  $('#henergybar').prop('style',`width: ${Math.floor(heroinfo['currenergy']/heroinfo['maxenergy'])}`)
+  $('#hdmg').text(`Damage: ${heroinfo['basedmg']}+${heroinfo['dmgvariance'][0]}d${heroinfo['dmgvariance'][1]}`);
+  $('#htype').text(`Type: ${heroinfo['type']}`);
+  if (heroinfo['maxarmor'] === heroinfo['currarmor']) {
+    $('#harmor').text(`Armor: ${heroinfo['currarmor']}`);
+  } else {
+      $('#harmor').text(`Armor: ${heroinfo['currarmor']}/${heroinfo['maxarmor']}`);
+  }
+  if (heroinfo['maxward'] === 0) {
+    $('#hward').text(``);
+  } else if (heroinfo['maxward'] === heroinfo['currward']) {
+    $('#hward').text(`Ward: ${heroinfo['currarmor']}`);
+  } else {
+      $('#hward').text(`Ward: ${heroinfo['currward']}/${heroinfo['maxward']}`);
+  }
+}
